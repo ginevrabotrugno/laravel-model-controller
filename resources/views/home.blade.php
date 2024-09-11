dc02d8260bed9ca60fa5e8efc0bc8e5a5f76e39a
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-09-11 15:40:42 +0200

    feat: print movies in cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,22 +3,27 @@
 
 @section('content')
 
-{{-- @dump($movies) --}}
-
 <h1 class="text-center">
     HOME
 </h1>
 
 <div class="container d-flex justify-content-between flex-wrap">
 
-    <div class="card mt-5" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+    @foreach ($movies as $movie)
+
+        <div class="card mt-5" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title"> {{ $movie->title }} </h5>
+                <h6 class="card-subtitle mb-2 text-muted"> {{ $movie->original_title }} </h6>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item"> {{ $movie->nationality }} </li>
+                <li class="list-group-item"> {{ $movie->vote }} </li>
+            </ul>
         </div>
-    </div>
+
+
+    @endforeach
 
 
 
